8c37a3d41801bf07d0f8557141d2125b7c99d1d9
Nikspb2022 <dklsgj@gmail.com>
2024-01-23 21:40:21 +0600

    моделс

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routers import auth, todos, users
+import models
+from database import engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
